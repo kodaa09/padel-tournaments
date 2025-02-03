8689d3bea54acd37f88d77664e9b91650ecc9e02
kodaa <je.elbaze@gmail.com>
2025-02-02 20:54:56 -0500

    do tournament migration

---
+++ backend/database/migrations/1738546870866_create_tournaments_table.ts
@@ -0,0 +1,27 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'tournaments'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.string('start_date').notNullable()
+      table.string('end_date').notNullable()
+      table.string('location').notNullable()
+      table.enu('status', ['upcoming', 'ongoing', 'completed']).defaultTo('upcoming')
+      table
+        .enu('diffciulty', ['P25', 'P100', 'P250', 'P500', 'P1000', 'P2000', 'any'])
+        .defaultTo('any')
+      table.enu('categorie', ['men', 'women', 'mixte']).defaultTo('any')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
